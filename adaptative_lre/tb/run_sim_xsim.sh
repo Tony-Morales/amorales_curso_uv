56cc0d3be07f06c78fa22e33d8718936d447e7d7
tony <antonioamoraless30@gmail.com>
2026-08-16 16:30:39 +0200

    add xrun simulator

--- adaptative_lre/tb/run_sim_xsim.sh
+++ adaptative_lre/tb/run_sim_xsim.sh
@@ -15,9 +15,7 @@ TEST_FILE="${TEST_NAME}.txt"
 NB_DATA=${2:-8}
 NB_COUNT=${3:-8}
 
-# ******************************************************************************
 # File and Top Module Definitions
-# ******************************************************************************
 TB_TOP="tb_adaptive_lre"
 SIM_DIR="sim_build/$TEST_NAME"
 TIMESCALE="1ns/1ps"
@@ -34,8 +32,11 @@ RTL_FILES=(
 )
 
 echo "**********************************************************"
-echo " Starting Simulation with Vivado XSim"
-echo " Top Module: ${TB_TOP}"
+echo "Starting Simulation with Vivado XSim"
+echo "Top Module: ${TB_TOP}"
+echo " NB_DATA: ${NB_DATA}"
+echo " NB_COUNT: ${NB_COUNT}"
+echo " Vector: ${TEST_FILE}"
 echo "**********************************************************"
 
 # Create and clean temporary simulation directory
@@ -57,7 +58,7 @@ done
 if [ -f "../tb/vectors/$TEST_FILE" ]; then
     cp "../tb/vectors/$TEST_FILE" "./$SIM_DIR/input.txt"
 else
-    echo "Error: Test file ../tb/vectors/$TEST_FILE not found!"
+    echo "Error: Test file ../tb/vectors/$TEST_FILE not found"
     exit 1
 fi
 
