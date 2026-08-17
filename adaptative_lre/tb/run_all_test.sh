8102741ed397c6a6f797ac74c04042db0d1603d9
tony <antonioamoraless30@gmail.com>
2026-08-17 17:53:28 +0200

    update test list

--- adaptative_lre/tb/run_all_test.sh
+++ adaptative_lre/tb/run_all_test.sh
@@ -22,8 +22,6 @@ $RUN_CMD vector_ovf_nb_data_32_nb_count_16  32  16
 $RUN_CMD vector_ovf_nb_data_32_nb_count_8   32  8
 $RUN_CMD vector_ovf_nb_data_8_nb_count_8    8   8
 
-$RUN_CMD vector_hand_rx_2_fullsize          8  8 
-$RUN_CMD vector_hand_rx_2_resize            8  8 
 $RUN_CMD vector_hand_rx_fullsize            8  8 
 $RUN_CMD vector_hand_rx_resize              8  8 
 $RUN_CMD vector_knee_rx_fullsize            8  8 
